5b8d289b192ffde1fa1f0fc669159b04e399c224
Ahmad-Rizki21 <mendungriski135@gmail.com>
2025-01-02 20:09:27 +0700

    Ganti Name

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="main-content">
     <section class="section">
         <div class="section-header">
-            <h1>Dashboard</h1>
+            <h1>Dashboard FTTH Ticketing System</h1>
         </div>
 
         <div class="section-body">
