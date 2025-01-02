4400331d0b0ccef03b52d809b2b8cb168ac83a06
Ahmad-Rizki21 <mendungriski135@gmail.com>
2025-01-02 20:21:45 +0700

    Fix Warning

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -5,61 +5,42 @@ namespace App\Http\Controllers;
 use App\Models\Sla;
 use App\Models\User;
 use App\Models\Ticket;
-use App\Mail\MailNotify;
 use App\Models\Customer;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
 use App\Providers\LogActivity as ProvidersLogActivity;
 
 class TicketController extends Controller
 {
-
-
-      /**
-     * __construct
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware(['permission:tickets.index|tickets.create|tickets.edit|tickets.delete']);
     }
-    
-    
 
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-
-
-        if(Auth::user()->hasRole('Teknisi'))
-        {
-            $tickets = Ticket::where('assignee', Auth::id())->latest()->when(request()->q, function($tickets) {
-                $tickets = $tickets->where('problemsummary', 'like', '%'. request()->q . '%');
+        $tickets = Auth::user()->hasRole('Teknisi')
+            ? Ticket::where('assignee', Auth::id())->latest()->when(request()->q, function ($tickets) {
+                $tickets->where('problemsummary', 'like', '%' . request()->q . '%');
+            })->paginate(5)
+            : Ticket::latest()->when(request()->q, function ($tickets) {
+                $tickets->where('problemsummary', 'like', '%' . request()->q . '%');
             })->paginate(5);
-        } else {
-            $tickets = Ticket::latest()->when(request()->q, function($tickets) {
-                $tickets = $tickets->where('problemsummary', 'like', '%'. request()->q . '%');
-            })->paginate(5);
-        }
-        
 
         return view('tickets.index', compact('tickets'));
-
-       
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
@@ -72,117 +53,80 @@ class TicketController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
-{
-    // Validasi input
-    $this->validate($request, [
-        'updated_customer' => 'required',
-        'reporteddate' => 'required|date',
-        'sla_id' => 'required',
-        'summary' => 'required',
-        'detail' => 'required',
-        'technician_id' => 'required',
-    ]);
+    {
+        $this->validate($request, [
+            'updated_customer' => 'required',
+            'reporteddate' => 'required|date',
+            'sla_id' => 'required',
+            'summary' => 'required',
+            'detail' => 'required',
+            'technician_id' => 'required',
+        ]);
 
-    $year = today()->year;
+        $year = today()->year;
+        $latest_ticket = Ticket::latest()->first();
+        $new_number = $latest_ticket ? (int)Str::of($latest_ticket->number)->explode('/')->get(0) + 1 : 1;
 
-    // Mendapatkan tiket terbaru
-    $latest_ticket = Ticket::latest()->first();
+        $ticket = Ticket::create([
+            'number' => "$new_number/$year",
+            'sla_id' => $request->input('sla_id'),
+            'reportedby' => Auth::id(),
+            'customer_id' => $request->input('updated_customer'),
+            'reporteddate' => $request->input('reporteddate'),
+            'problemsummary' => $request->input('summary'),
+            'problemdetail' => $request->input('detail'),
+            'status' => 'Open',
+            'assignee' => $request->input('technician_id'),
+            'assigneddate' => now(),
+        ]);
 
-    // Menentukan nomor tiket baru
-    if ($latest_ticket) {
-        $last_number = Str::of($latest_ticket->number)->explode('/');
-        $new_number = (int)$last_number->get(0) + 1;
-    } else {
-        $new_number = 1; // Nomor awal jika tabel kosong
-    }
-
-    // Membuat tiket baru
-    $ticket = Ticket::create([
-        'number' => $new_number . "/" . $year,
-        'sla_id' => $request->input('sla_id'),
-        'reportedby' => Auth::id(),
-        'customer_id' => $request->input('updated_customer'),
-        'reporteddate' => $request->input('reporteddate'),
-        'problemsummary' => $request->input('summary'),
-        'problemdetail' => $request->input('detail'),
-        'status' => 'Open',
-        'assignee' => $request->input('technician_id'),
-        'assigneddate' => now(),
-    ]);
-
-    // Menyimpan tiket
-    $ticket->save();
-
-    // Mengembalikan respon (jika dibutuhkan)
-    // return response()->json([
-    //     'message' => 'Ticket created successfully',
-    //     'ticket' => $ticket,
-    // ], 201);
-
-         
-
-         if($ticket){
-            //redirect dengan pesan sukses
-            $technician = User::findOrFail($request->input('technician_id'));
-            $user = Auth::user();
-            $subject = 'Membuat Tiket No '. $ticket->number;
-            event(new ProvidersLogActivity($user, $subject));
-            // Mail::to($technician->email)->queue(new MailNotify($ticket));
-            // dispatch(new SendEmailJob($technician->email, $ticket));
+        if ($ticket) {
+            event(new ProvidersLogActivity(Auth::user(), 'Membuat Tiket No ' . $ticket->number));
             return redirect()->route('tickets.index')->with(['success' => 'Tiket Berhasil Dibuat']);
-            
-        }else{
-        //     //redirect dengan pesan error
-            return redirect()->route('tickets.index')->with(['error' => 'Tiket Gagal Dibuat']);
         }
+        return redirect()->route('tickets.index')->with(['error' => 'Tiket Gagal Dibuat']);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function show($id)
     {
-        //
+        $ticket = Ticket::findOrFail($id);
+        return view('tickets.show', compact('ticket'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-
-    //  public function edit($id)
-    // {
-    //     $ticket = Ticket::find($id);
-    //     $customers = Customer::all(); // Mengambil daftar customer
-    //     return view('tickets.edit', compact('ticket', 'slas', 'users', 'customer')); // Mengirimkan data ke view
-    // }
     public function edit($id)
     {
         $ticket = Ticket::findOrFail($id);
         $slas = Sla::orderBy('name', 'asc')->get();
         $users = User::role('teknisi')->get();
-        $customer = new Customer;
+        $customer = Customer::all();
         return view('tickets.edit', compact('ticket', 'slas', 'users', 'customer'));
-    }   
+    }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'updated_customer' => 'required',
             'reporteddate' => 'required|date',
             'sla_id' => 'required',
@@ -192,55 +136,31 @@ class TicketController extends Controller
         ]);
 
         $ticket = Ticket::findOrFail($id);
+        $ticket->update($request->only([
+            'sla_id', 'customer_id', 'reporteddate', 'problemsummary', 'problemdetail', 'assignee', 'resolution', 'status'
+        ]));
 
-        $ticket->update([
-            'sla_id' => $request->input('sla_id'),
-            'customer_id' => $request->input('updated_customer'),
-            'reporteddate' => $request->input('reporteddate'),
-            'problemsummary' => $request->input('summary'),
-            'problemdetail' => $request->input('detail'),
-            'assignee' => $request->input('technician_id'),
-            'resolution' => $request->input('resolution'),
-            'status' => $request->input('status')
-         
-         ]);
-
-         if($ticket){
-            //redirect dengan pesan sukses
-            $user = Auth::user();
-            $subject = 'Memperbaharui Tiket No '. $ticket->number;
-            event(new ProvidersLogActivity($user, $subject));
+        if ($ticket) {
+            event(new ProvidersLogActivity(Auth::user(), 'Memperbaharui Tiket No ' . $ticket->number));
             return redirect()->route('tickets.index')->with(['success' => 'Tiket Berhasil Diupdate!']);
-        }else{
-            //redirect dengan pesan error
-            return redirect()->route('tickets.index')->with(['error' => 'Tiket Gagal Diupdate!']);
         }
-
-
+        return redirect()->route('tickets.index')->with(['error' => 'Tiket Gagal Diupdate!']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
         $ticket = Ticket::findOrFail($id);
-        $ticket->delete();
 
-        if($ticket){
-            $user = Auth::user();
-            $subject = 'Menghapus Tiket No '. $ticket->number;
-            event(new ProvidersLogActivity($user, $subject));
-            return response()->json([
-                'status' => 'success'
-            ]);
-        }else{
-            return response()->json([
-                'status' => 'error'
-            ]);
+        if ($ticket->delete()) {
+            event(new ProvidersLogActivity(Auth::user(), 'Menghapus Tiket No ' . $ticket->number));
+            return response()->json(['status' => 'success']);
         }
+        return response()->json(['status' => 'error']);
     }
 }
